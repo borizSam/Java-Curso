d7def18e8b2a1c159590981d0629ce767cfe7bb0
boris <bhuarachiperez@gmail.com>
2021-10-10 11:18:54 -0700

    Completado

---
+++ 00_05_presentemnos/Mepresento.java
@@ -0,0 +1,6 @@
+/*Programa que nos presenta */
+public class Mepresento {
+    public static void main(String[] args) {
+        System.out.println("Hola, soy Boris, el futuro mejor desarrollador del Mundo!");
+    }
+}
