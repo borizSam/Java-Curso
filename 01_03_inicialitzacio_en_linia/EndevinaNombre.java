635630619f49b97d8384af50c131f1199e245075
borizSam <bhuarachiperez@gmail.com>
2021-10-17 18:29:16 -0700

    finalitzat l’exercici 01_03

---
+++ 01_03_inicialitzacio_en_linia/EndevinaNombre.java
@@ -0,0 +1,43 @@
+/*
+     * Aquest programa mostra capacitats d'endevinació
+     *
+     * Per executar-ho, fes
+     * $ java EndevinaNombre 42
+     */
+
+    public class EndevinaNombre {
+
+        public static void main(String[] args) {
+
+            int numeroPensat;   // guardarà el número pensat
+            int numero;         // guardarà els càlculs intermitjos
+
+            // pensa un número
+            numeroPensat = Integer.parseInt(args[0]);
+            System.out.print("El número pensat és ");
+            System.out.print(numeroPensat);
+
+            // Multiplica'l per 3
+            numero = numeroPensat * 3;
+            System.out.print(" **Quan el multipliques per 3 obtens ");
+            System.out.print(numero);
+
+            // Suma-li 6
+            numero = numero + 6;
+            System.out.print(" **Quan li sumes 6 arriba a ");
+            System.out.print(numero);
+
+            // divideix-ho tot per 3
+            numero = numero / 3;
+            System.out.print(" **Un cop dividit per 3 queda ");
+            System.out.print(numero);
+
+            // resta-li el número pensat
+            numero = numero - numeroPensat;
+            System.out.print(" **Finalment, en restar-li el valor inicial, queda ");
+            System.out.print(numero);
+
+            // compara el resultat
+            System.out.print(" **A que el resultat és 2?");
+        }
+    }
