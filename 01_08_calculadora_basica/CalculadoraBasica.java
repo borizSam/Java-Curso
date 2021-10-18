233828c6a88125e8a7c4b4d385bc30688e423a0d
borizSam <bhuarachiperez@gmail.com>
2021-10-18 16:10:41 +0200

    finalizat l'exercici 01_08

--- 01_08_calculadora_basica/CalculadoraBasica.java
+++ 01_08_calculadora_basica/CalculadoraBasica.java
@@ -26,6 +26,6 @@ public class CalculadoraBasica {
         System.out.println(primerOperand + " + " + segonOperand + " = " + suma);
         System.out.println(primerOperand + " - " + segonOperand + " = " + resta);
         System.out.println(primerOperand + " * " + segonOperand + " = " + multiplicacio);
-  	System.out.println(primerOperand + " / " + segonOperand + " / " + divisio);
+  	System.out.println(primerOperand + " / " + segonOperand + " = " + divisio);
   }
 }
