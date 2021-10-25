b5a53bb73d83b018a59c56e527ac80a0d62b52ea
boris <bhuarachiperez@gmail.com>
2021-10-25 03:23:47 +0200

    Beta-version

---
+++ 01_21_tresenorde/TresEnOrdre.java
@@ -0,0 +1,15 @@
+public class TresEnOrdre{
+	public static void main(String[]args){
+		System.out.println("Primer?");
+		int a = Integer.parseInt(Entrada.readLine());
+		System.out.println("Segon?");
+		int b = Integer.parseInt(Entrada.readLine());
+		System.out.println("Tercer?");
+		int c = Integer.parseInt(Entrada.readLine());
+		if (a>b && a>c){
+			System.out.println(a+", "+b+" i "+c);
+		}else if (b>a && b>c){
+			System.out.println(b+", "+)
+		}
+	}
+}
