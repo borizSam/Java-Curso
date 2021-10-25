951eb0007378e730087e9eb2557e742f824b2723
boris <bhuarachiperez@gmail.com>
2021-10-26 00:51:17 +0200

    finalizat l'exercici_01_25

---
+++ 01_25_nombre_de_fills/Fills.java
@@ -0,0 +1,17 @@
+public class Fills {
+	public static void main (String[]args){
+		System.out.println("Quants fills tens?");
+		int fills = Integer.parseInt(Entrada.readLine());
+		if (fills < 0){
+		System.out.println("No pots tenir menys de 0 fills!");
+		}else if (fills == 0){
+		System.out.println("Tot el que t'has estalviat en bolquers!");
+		}else if (fills > 1 && fills < 5){
+		System.out.println("No t'avorreixes a casa,eh?");
+		}else if (fills == 1){
+		System.out.println("Compte de no mimar-lo massa!");
+		}else if (fills >= 5){
+		System.out.println("Tu si fas país!");
+		}
+	}
+}
