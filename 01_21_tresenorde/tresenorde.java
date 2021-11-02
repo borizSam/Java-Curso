5b1812e3bdef2d2f5ca5758cbd11a9664e4f5267
Boris Ivan <bhuarachiperez@gmail.com>
2021-11-03 00:59:29 +0100

    Finalizado

--- 01_21_tresenorde/tresenorde.java
+++ 01_21_tresenorde/tresenorde.java
@@ -1,5 +1,5 @@
 /*Ordenar 3 numeros de menor a mayor introducidos por el usuario */
-public class TresEnOrdre{
+public class tresenorde{
 	public static void main(String[]args){
 		System.out.println("Primer?");
 		int a = Integer.parseInt(Entrada.readLine());
