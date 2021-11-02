fc067404fb281ef4a60d4cc0858d8ac09b2c5853
borizSam <bhuarachiperez@gmail.com>
2021-11-02 20:39:22 +0100

    Finalizado

--- 01_14_preso/Preso.java
+++ 01_14_preso/Preso.java
@@ -9,7 +9,7 @@ public class Preso {
         System.out.println("Quants anys teniu?");
         int edat = Integer.parseInt(Entrada.readLine());
         if (edat >= 0 && edat <=18 ) {
-            System.out.println("Vos ja podeu anar a la presó! ");
+            System.out.println("Vos ja podeu anar a la presó!");
         } else {
             System.out.println("Aneu amb compte "+ nom);
 	}
