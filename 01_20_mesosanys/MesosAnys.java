b37df11e4af615dbba78ec1cba2878c799be2e71
Boris Ivan <bhuarachiperez@gmail.com>
2021-11-03 00:45:21 +0100

    Finalizado

--- 01_20_mesosanys/MesosAnys.java
+++ 01_20_mesosanys/MesosAnys.java
@@ -9,14 +9,15 @@ public class MesosAnys {
 		int anterior = mes - 1;
 		int posterior = mes + 1;
 		int mesos= mes - 11;
+		int mese=mes +11;
 		int anys= any+1;
 		int anyss=any-1;
-		if ( mes <= 11){  /*rango de meses correctos*/
+		if ( mes >=2 && mes <= 11){  /*rango de meses correctos*/
 			System.out.println("Anterior "+anterior+"/"+any+" i posterior "+posterior+"/"+any);
 		}else if (mes==12){
 			System.out.println("Anterior "+anterior+"/"+any+" i posterior "+mesos+"/"+anys);
 		}else if (mes==1){
-			System.out.println("Anterior "+anterior+"/"+anyss+" i posterior "+posterior+"/"+any);
+			System.out.println("Anterior "+mes+"/"+anyss+" i posterior "+posterior+"/"+any);
 		}
 	}
 }
