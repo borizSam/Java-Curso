53f1aca6e534a65d455cf721a220e7120910b479
Boris Ivan <bhuarachiperez@gmail.com>
2021-11-03 20:53:14 +0100

    Finalizado

--- 01_25_nombre_de_fills/Fills.java
+++ 01_25_nombre_de_fills/Fills.java
@@ -1,3 +1,12 @@
+/*Programa que pide al usuario nombre de hijos que tiene menys de 0: No pots tenir menys de 0 fills!
+
+0: Tot el que t’has estalviat en bolquers!
+
+1: Compte de no mimar-lo massa!
+
+entre 1 i 5 (no inclosos): No t’avorreixes a casa, eh?
+
+més de 4: Tu sí fas país! */
 public class Fills {
 	public static void main (String[]args){
 		System.out.println("Quants fills tens?");
@@ -7,7 +16,7 @@ public class Fills {
 		}else if (fills == 0){
 		System.out.println("Tot el que t'has estalviat en bolquers!");
 		}else if (fills > 1 && fills < 5){
-		System.out.println("No t'avorreixes a casa,eh?");
+		System.out.println("No t'avorreixes a casa, eh?");
 		}else if (fills == 1){
 		System.out.println("Compte de no mimar-lo massa!");
 		}else if (fills >= 5){
