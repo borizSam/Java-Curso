8369b092fad35c38918655881a5303d3dfb35d1d
Boris Ivan <bhuarachiperez@gmail.com>
2021-11-03 20:49:56 +0100

    Finalizado

--- 01_24_semafor/Semafor.java
+++ 01_24_semafor/Semafor.java
@@ -1,3 +1,4 @@
+/*Programa que que pide un colo al usuario rojo marillo o verde si esta en rojo le dice que espere si esta en verde que pase si esta en amarillo que corra  */
 public class Semafor {
 	public static void main (String[] args){
 		System.out.println("Color?");
