a1eb2c3bec350ba832d5bd0151b58bd97d5affb1
Boris Ivan <bhuarachiperez@gmail.com>
2021-11-03 20:38:45 +0100

    Finalizado

--- 01_23_pagaments/Pagament.java
+++ 01_23_pagaments/Pagament.java
@@ -1,10 +1,19 @@
-public class Pagament{
+/*Programa que calcula el precio si falta o sobra */
+public class Pagament {
 	public static void main (String[]args){
 	System.out.println("Preu?");
-	int preu = Integer.parseInt(Entrada.readLine());
-	System.out.println("Paga?");
-	int paga = Integer.parseInt(Entrada.readLine());
-	int suma= preu - paga;
+	int preu=Integer.parseInt(Entrada.readLine());
+	System.out.println("paga?");
+	int paga=Integer.parseInt(Entrada.readLine());
+	int suma= preu - paga; /*Calculamos el resultado*/
+	int resultado= Math.abs(suma); /*Convertimos el resultado en positivo*/
 	System.out.println(suma);
+	if (suma>1){
+		System.out.println("Falten "+resultado+"€");
+	}else if (suma==0){
+		System.out.println("No sobra ni falta res");
+	}else{
+		 System.out.println("Sobren "+resultado+"€");
+	}
 	}
 }
