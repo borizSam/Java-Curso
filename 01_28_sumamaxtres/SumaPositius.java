f933d009e9001b298b51fe1309379b791e77fcdd
Boris Ivan <bhuarachiperez@gmail.com>
2021-11-08 00:04:14 +0100

    Finalizado

--- 01_28_sumamaxtres/SumaPositius.java
+++ 01_28_sumamaxtres/SumaPositius.java
@@ -1,15 +1,32 @@
 /*sumar tres positivos*/
 public class SumaPositius{
     public static void main(String[] args) {
-	System.out.println("Introdueix el primer valor");
-	int nota=Integer.parseInt(Entrada.readLine());
-	int suma=0;
-		while (nota >=0){
-			suma = suma+nota;
-			System.out.println("Introdueix el primer valor");
-			nota=Integer.parseInt(Entrada.readLine());
+        // creem la variable on guardarem el resultat de sumar
+        int suma = 0;   // inicialment no hem sumat res i per tant és 0
 
-		}
-		System.out.println("La suma és "+suma);
+        // declarem la variable que contindrà els valors llegits
+        int valor;
+
+        // processem el primer valor
+        System.out.println("Introdueix un valor");
+        valor = Integer.parseInt(Entrada.readLine());
+        if (valor >= 0) {
+            suma = suma + valor;
+
+            // processem el segon valor
+            System.out.println("Introdueix un valor");
+            valor = Integer.parseInt(Entrada.readLine());
+            if (valor >= 0) {
+                suma = suma + valor;
+            }
+            	// processem el segon valor
+           	System.out.println("Introdueix un valor");
+            	valor = Integer.parseInt(Entrada.readLine());
+            	if (valor >= 0) {
+                	suma = suma + valor;
+            	}
+        }
+        // mostrem el resultat
+        System.out.println("La suma és " + suma);
     }
 }
