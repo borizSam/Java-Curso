9f9a67b7a23cf99ba672f2ae116f73ffddc26324
Boris Ivan <bhuarachiperez@gmail.com>
2021-11-07 16:55:46 +0100

    Por acabar

--- 01_31_lloro/Lloro.java
+++ 01_31_lloro/Lloro.java
@@ -1,9 +1,8 @@
 /*Programa que pide al usuario una palabra y el loro lo repite cuando el usiario introduza un spacio el programa termina yse despide*/
 public class Lloro {
 	public static void main (String[]args){
-	String a=s;
-	String space= a.isEmpty();
-		while (!space.equals("")){
+	String space="";
+		while (space.isBlank()==true){
 			System.out.println("El lloro espera paraula:");
 			space =Entrada.readLine();
 			System.out.println("El lloro repeteix: "+space);
