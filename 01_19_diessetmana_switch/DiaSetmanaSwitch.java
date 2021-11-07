8a53c0f268078e4c58da2d7602b8f636563b3ed8
Boris Ivan <bhuarachiperez@gmail.com>
2021-11-07 01:08:28 +0100

    Finalizado

--- 01_19_diessetmana_switch/DiaSetmanaSwitch.java
+++ 01_19_diessetmana_switch/DiaSetmanaSwitch.java
@@ -1,24 +1,26 @@
+/*Dia de la semana por parametro segun los numeros del 1 al 7 si pone otro numero error  */
+
 public class DiaSetmanaSwitch {
 	public static void main(String[] args){
 	int dia = Integer.parseInt(args[0]);
 	String diaString;
 	switch (dia)
 	{
-		case 1: diaString = "Lunes";
+		case 1: diaString = "Dilluns";
 			break;
-		case 2: diaString = "Martes";
+		case 2: diaString = "Dimarts";
                         break;
-		case 3: diaString = "Miercoles";
+		case 3: diaString = "Dimecres";
                         break;
-		case 4: diaString = "Jueves";
+		case 4: diaString = "Dijous";
                         break;
-		case 5: diaString = "Viernes";
+		case 5: diaString = "Divendres";
                         break;
-		case 6: diaString = "Sabado";
+		case 6: diaString = "Dissabte";
                         break;
-		case 7: diaString = "Domingo";
+		case 7: diaString = "Diumenge";
                         break;
-		default: diaString = "Error!!";
+		default: diaString = "Error";
 			break;
 	}
 	System.out.println(diaString);
