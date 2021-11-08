56cdf0bce6e92e1e6ffd405c39c30d453dd6abae
borizSam <bhuarachiperez@gmail.com>
2021-11-08 17:23:36 +0100

    2

--- 01_35_endevina/Endivina.java
+++ 01_35_endevina/Endivina.java
@@ -3,16 +3,16 @@ public class Endivina{
 	public static void main (String[]args){
 	//System.out.println("Introdueix un nombre entre el 1 i el 100");
 	int num=1;
-		while (num!=42){
+		while (num!=42 ){
 			System.out.println("Introdueix un nombre entre el 1 i el 100");
 			num=Integer.parseInt(Entrada.readLine());
-			if (num>100){
+			if (num>100 ){
 				System.out.println("Com a màxim 100");
-			}else if (num==0){
+			}else if (num==0 ){
 				System.out.println("Com a mínim 1");
-			}else if (num >=50){
+			}else if (num > 42 ){
 				System.out.println("És més petit que "+num);
-			}else if (num>1){
+			}else if (num > 1 ){
 				System.out.println("És més gran que "+num);
 			}
 		}
