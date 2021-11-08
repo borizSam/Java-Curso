ddf08c0672eeb02c45cd4a994ba6869b5092e196
Boris Ivan <bhuarachiperez@gmail.com>
2021-11-08 22:59:27 +0100

    Finalizado

--- 01_32_trobamaxim/TrobaMaxim.java
+++ 01_32_trobamaxim/TrobaMaxim.java
@@ -1,8 +1,8 @@
 /*Encontrar el valor maximo de los numeros introducidos cuando el usuario introduzca un numero negativo el rpograma acabara y nos enseñara un numero mayor */
 public class TrobaMaxim {
 	public static void main (String[]args){
-	System.out.println("Introdueix un valor");
-	int valor=Integer.parseInt(Entrada.readLine());
+	//System.out.println("Introdueix un valor");
+	int valor=1;
 	int maxim=0;
 		while(valor>0){
 			System.out.println("Introdueix un valor");
