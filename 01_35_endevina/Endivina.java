677bdd1905215ab44a65c6d2de71e7f4e8b69ce1
borizSam <bhuarachiperez@gmail.com>
2021-11-08 17:03:26 +0100

    FInalizado

--- 01_35_endevina/Endivina.java
+++ 01_35_endevina/Endivina.java
@@ -1,20 +1,21 @@
 /*Adivina el numero pensado */
 public class Endivina{
 	public static void main (String[]args){
-	System.out.println("Introdueix un nombre entre el 1 i el 100");
-	int num=Integer.parseInt(Entrada.readLine());
+	//System.out.println("Introdueix un nombre entre el 1 i el 100");
+	int num=1;
 		while (num!=42){
+			System.out.println("Introdueix un nombre entre el 1 i el 100");
 			num=Integer.parseInt(Entrada.readLine());
-			if (num>=1 && num<=5){
-				System.out.println("És més gran que ");
-			}else if (num>5 && num<=50){
-				System.out.println("És més petit que "+num);
+			if (num>100){
+				System.out.println("Com a màxim 100");
 			}else if (num==0){
 				System.out.println("Com a mínim 1");
-			}else if (num>1 && num <=100){
-				System.out.println("Com a màxim 100");
+			}else if (num >=50){
+				System.out.println("És més petit que "+num);
+			}else if (num>1){
+				System.out.println("És més gran que "+num);
 			}
+		System.out.println("Has encertat!");
 		}
-	System.out.println("Has encertat!");
 	}
 }
