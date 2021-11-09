7c4a875010bccbda116b0a1413a04998daf10d9c
Boris Ivan <bhuarachiperez@gmail.com>
2021-11-09 01:42:36 +0100

    FInalizado

---
+++ 01_38_deu_primers_decreixent/DeuPrimersDecreixent.java
@@ -0,0 +1,10 @@
+/*Programa que nos enseña los nuemeros del 10 al 1*/
+public class DeuPrimersDecreixent {
+	public static void main (String[]args){
+	int contador=11;
+		while (contador>1 && contador<12){
+			contador=contador-1;
+			System.out.println(contador);
+		}
+	}
+}
