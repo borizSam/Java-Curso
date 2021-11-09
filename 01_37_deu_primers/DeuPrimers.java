531e8f9819d083cea251e011ad66f1c4378dcd50
Boris Ivan <bhuarachiperez@gmail.com>
2021-11-09 01:29:38 +0100

    Finalizado

---
+++ 01_37_deu_primers/DeuPrimers.java
@@ -0,0 +1,10 @@
+/*Programa que nos enseña los 10 primeros numeros con un while*/
+public class DeuPrimers {
+	public static void main (String[]args){
+	int contador=0;
+		while(contador<10){
+			contador=contador+1;
+			System.out.println(contador);
+		}
+	}
+}
