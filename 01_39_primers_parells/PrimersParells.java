31d739252a4105913a2a26db30f68f1e4b926b6e
Boris Ivan <bhuarachiperez@gmail.com>
2021-11-09 01:55:00 +0100

    Finalizado

---
+++ 01_39_primers_parells/PrimersParells.java
@@ -0,0 +1,10 @@
+/*Programa que nos enseña los numeros pares de 10 numeros*/
+public class PrimersParells{
+	public static void main (String[]args){
+	int contador=0;
+		while(contador<10){
+			contador=contador+2;
+			System.out.println(contador);
+		}
+	}
+}
