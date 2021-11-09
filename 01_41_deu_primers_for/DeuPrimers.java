d64c7fcc7bfe7c3db4915b0605ad9e98ddb11189
borizSam <bhuarachiperez@gmail.com>
2021-11-09 20:44:27 +0100

    Finalizado

---
+++ 01_41_deu_primers_for/DeuPrimers.java
@@ -0,0 +1,9 @@
+/*Enseña los 10 primeros numeros con un for */
+public class DeuPrimers {
+	public static void main(String[]args){
+		for (int contador=1; contador<=10; contador=contador+1){
+			System.out.println(contador);
+		
+		}
+	}
+} 
