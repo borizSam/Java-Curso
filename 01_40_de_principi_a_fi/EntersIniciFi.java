938bb74e9b71d45522a706a8d92269850944846d
Boris Ivan <bhuarachiperez@gmail.com>
2021-11-09 02:19:55 +0100

    PorResolver

---
+++ 01_40_de_principi_a_fi/EntersIniciFi.java
@@ -0,0 +1,13 @@
+/*El programa pedira tres valores al usuario valor inicial y el valor final el primer valor siempre sera menor al 2do valor y el usuario nos introducira un salto*/
+public class EntersIniciFi{
+	public static void main (String []args){
+	System.out.println("Valor inicial?");
+	int inicial=Integer.parseInt(Entrada.readLine());
+	System.out.println("Valor final?");
+	int finas=Integer.parseInt(Entrada.readLine());
+	System.out.println("Salt?");
+	int salto=Integer.parseInt(Entrada.readLine());
+	int contador=0;
+
+	}
+}
