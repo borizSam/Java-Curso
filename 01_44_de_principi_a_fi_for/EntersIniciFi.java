c019baae2d80d2ab25824129f055ea4ef8f761b7
Boris Ivan <bhuarachiperez@gmail.com>
2021-11-18 01:57:55 +0100

    FInalizado

---
+++ 01_44_de_principi_a_fi_for/EntersIniciFi.java
@@ -0,0 +1,15 @@
+/*Incicial final y el salto */
+public class EntersIniciFi{
+	public static void main (String[]args){
+	System.out.println("Valor inicial?");
+	int inicial=Integer.parseInt(Entrada.readLine());
+	System.out.println("Valor final?");
+	int final1=Integer.parseInt(Entrada.readLine());
+	System.out.println("Salt?");
+	int salto=Integer.parseInt(Entrada.readLine());
+	for (inicial=inicial; inicial<=final1 && salto>0; inicial=inicial+salto){
+		System.out.println(inicial);
+	}
+	
+	}
+}
