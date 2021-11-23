f840074d8d63f1a88daa348780da309995476454
Boris Ivan <bhuarachiperez@gmail.com>
2021-11-23 19:02:11 +0100

    Finalizdo

--- 01_49_triangle_nombres/TriangleNombres.java
+++ 01_49_triangle_nombres/TriangleNombres.java
@@ -3,12 +3,15 @@ public class TriangleNombres{
 	public static void main (String[]args){
 	System.out.println("Nombre?");
 	int num=Integer.parseInt(Entrada.readLine());
+	if(num >0){
 		for (int linea=1; linea<=num; linea++){
 			for(int colum=1; colum<=linea; colum++){
-				int a=linea-colum;	
+				int a=linea-colum;
 				System.out.print(" "+a);
 			}
 			System.out.println();
 		}
+	}else{
+	}	
 	}
 }
