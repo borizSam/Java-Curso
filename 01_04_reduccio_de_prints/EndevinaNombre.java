688af12a443461744b19eb731b004a2d928a75a6
prgtest <bhuarachiperez@gmail.com>
2021-11-28 21:58:26 +0100

    prgtest autocommit #3 01_04: Finalizado

--- 01_04_reduccio_de_prints/EndevinaNombre.java
+++ 01_04_reduccio_de_prints/EndevinaNombre.java
@@ -15,19 +15,19 @@
             // pensa un número
             numeroPensat = Integer.parseInt(args[0]);
             // Multiplica'l per 3
-            numero = numeroPensat * 3;
+            int mult = numeroPensat * 3;
             // Suma-li 6
-            numero = numero + 6;
+            int suma = numeroPensat + 6;
             // divideix-ho tot per 3
-            numero = numero / 3;
+            int divi = numeroPensat / 3;
             // resta-li el número pensat
-            numero = numero - numeroPensat;
+            int rest = numeroPensat - numeroPensat;
             // compara el resultat
 	    System.out.println("El número pensat és " + numeroPensat);
-	    System.out.println("Quan el multipliques per 3 obtens " + numero);
-	    System.out.println("Quan li sumer 6 arriba a " + numero);
-	    System.out.println("Un cop dividit per 3 queda " + numero);
-	    System.out.println("Finalmente, en restar-li el valor inicial, queda " + numero);
+	    System.out.println("Quan el multipliques per 3 obtens " + mult);
+	    System.out.println("Quan li sumer 6 arriba a " + suma);
+	    System.out.println("Un cop dividit per 3 queda " + divi);
+	    System.out.println("Finalmente, en restar-li el valor inicial, queda " + rest);
 	    System.out.println("A que el resultat és 2?");
         }
     }
