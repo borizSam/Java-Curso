c662b368865b3dce9585e162671cd8b4d46abb6d
Boris Ivan <bhuarachiperez@gmail.com>
2021-12-02 01:27:34 +0100

    F

---
+++ 01_58_informecaracter/InformeCaracter.java
@@ -0,0 +1,9 @@
+/*Realizar un informe sovre los caracteres en java :D*/
+
+public class InformeCaracter{
+	public static void main (String[]args){
+	System.out.println("Caràcter?");
+	String caracter=Entrada.readLine();
+	System.out.println("Character.getName(1): "+"LATIN CAPITAL LETTER "+Character.getName('H'));
+	}
+}
