98516564ae6ae0d9dac479c9f2aa90ec0f00fe1e
borizSam <bhuarachiperez@gmail.com>
2021-12-13 17:26:57 +0100

    finalizado

--- 02_03_quadrat_reparametritzat/Quadrat.java
+++ 02_03_quadrat_reparametritzat/Quadrat.java
@@ -4,7 +4,8 @@ public class Quadrat{
 	public static void main (String[]args){
 	int costat=Integer.parseInt(args[0]);
 	char caracter=args[1].charAt(0);
-	dibuixaLinea(costat,caracter);
+	dibuixaQuadrat(costat,caracter);
+	dibuixaLinia(costat,caracter);
 	}
 	public static void dibuixaLinea(int costat, char caracter){
 		for (int filas=1; filas<=costat; filas++){
@@ -13,5 +14,17 @@ public class Quadrat{
 			}
 			System.out.println();
 		}	
+	}
+	public static void dibuixaQuadrat(int costat, char caracter){
+		for (int filas=1; filas<=costat; filas++){
+			dibuixaLinia(costat,caracter);
+		}
+		//System.out.println();
+	}
+	public static void dibuixaLinia(int costat, char caracter){
+		for (int colu=1; colu<=costat; colu++){
+				System.out.print(" "+caracter);
+			}
+			System.out.println();
 	}
 }
